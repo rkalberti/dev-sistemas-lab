a5dc9876f718148830a1062f5511374165e503aa
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-04-14 19:17:00 -0300

    Atualizar o pratica-um.py

--- primeiro-tri/pratica-um.py
+++ primeiro-tri/pratica-um.py
@@ -2,7 +2,6 @@
 # Objetivo: Criar sua autodescrição utilizando variáveis e função print().
 
 
-
 nome = "Ketlyn"
 idade = 16
 altura = 1.55
@@ -14,3 +13,4 @@ cor_olhos = "Azuis"
 curso = "Técnico em desenvolvimento de sistemas"
 hobby = "Andar de skate"
 
+print(f'Olá, meu nome é {nome}, tenho {idade} anos de idade, {altura} de altura, meu cabelo é {cor_cabelo},a minha pele é {cor_pele}, e meia olhos são {cor_olhos}. Atualmente sou estudante de {curso} e gosto de {hobby}.')
