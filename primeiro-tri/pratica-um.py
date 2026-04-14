de9661dbe90bfc2fe1ae0f1ed8053396a26f7d93
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-04-14 18:05:05 -0300

    Criar pratica-um.py

---
+++ primeiro-tri/pratica-um.py
@@ -0,0 +1,25 @@
+### Atividade 1 - Nivelamento ###
+# Objetivo: Criar sua autodescrição utilizando variáveis e função print().
+# 1 - Pesquise o que é autodescrição e para quê ela serve.
+# 2 - Crie as seguintes variáveis:
+nome = "Seu primeiro nome"
+idade = 999
+altura = 1.90
+genero = "Seu gênero"
+cor_de_pele = "..."
+cor_de_cabelo = "..."
+tamanho_cabelo = "..."
+estilo_cabelo = "..."
+# 3 - Crie 2 variáveis: uma armazena o curso que você faz, a outra armazena o seu hobby favorito.
+
+# 4 - Utilizando a função print(), mostre o texto completo da sua autodescrição, utilizando as variáveis que você criou!
+# Exemplo: "Olá, meu nome é Fulano, tenho 999 anos e 1.90 de altura... (continue até usar todas as variáveis)"
+
+## Forma de avaliação: ##
+# 0,5 ponto = Preenchimento correto das variáveis do item 2
+# 0,5 ponto = Criação das variáveis do item 3
+# 1,0 ponto = Coerência do texto e uso correto das variáveis na função print()
+# TOTAL: 2,0 pontos
+
+## Entrega: ##
+# O aluno deve criar um novo repositório no GitHub, com o nome "1TRI-DesenvolvimentoSistemas" e realizar upload do arquivo .py.
