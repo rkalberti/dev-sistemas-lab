02b1d25cd66a482293a6b908fda5a8545c17ceb5
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-04-14 17:58:59 -0300

    Criar aula-dois.py

---
+++ primeiro-tri/aula-dois.py
@@ -0,0 +1,10 @@
+#Aula 2, 
+
+contador = 1
+
+# ENQUANTO (se condição verdadeira): -> executa todo o código que está dentro do laço while
+# ENQUANTO (se condição falsa): -> para a execução
+
+while (contador <= 10):
+    print(contador)
+    contador = contador + 1
