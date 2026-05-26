b5be2e6cfb611a4ab19b8bf6170346e36554248b
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-05-26 14:56:02 -0300

    Atualizar o conta.py

--- segundo-tri/conta.py
+++ segundo-tri/conta.py
@@ -5,21 +5,64 @@ import random
 
 class Conta: 
     def __init__(self, titular, cpf, limite, chave_pix, senha):
-        # atributos:
+
+        # Propriedades
         self.__titular = titular
         self.__cpf = cpf
-        self.__agencia = 2026       # 2026 valor fixo, vai aparecer em todos os objetos
-        self.__conta = random.randint(100000, 999999)   # escolhe o número da conta 
+        self.__agencia = 2026
+        self.__conta = random.randint(100000, 999999)
         self.__saldo = 0
-        self.__limite = limite 
-        self.__chave_pix = chave_pix 
+        self.__limite = limite
+        self.__chave_pix = chave_pix
         self.__senha = senha
 
+    # Getter
+    @property
+    def titular(self):
+        return self.__titular
 
-    # Funçãao para mostra os dados
-    def mostrar(self):
-        print(f"Titular: {self.__titular}\nAgência: {self.__agencia}\nConta: {self.__conta}\nSaldo: {self.__saldo}")
+    # Setter
+    @titular.setter
+    def titular(self, novo_nome):
+        self.__titular = novo_nome
 
+    # Extrato
+    def extrato(self):
+        print(f"""
+Titular: {self.__titular}
+Agência: {self.__agencia}
+Conta: {self.__conta}
+Saldo: R${self.__saldo}
+""")
 
-    def saca(self, valor):
-        #verifica se pode sacar o valor
+    # Sacar
+    def sacar(self, valor):
+
+        if valor <= self.__saldo and valor > 0:
+            self.__saldo -= valor
+            print("Saque efetuado com sucesso!")
+            return True
+
+        else:
+            print("Não foi possível efetuar o saque!")
+            return False
+
+    # Depositar
+    def depositar(self, valor):
+
+        if valor > 0:
+            self.__saldo += valor
+            print("Depósito realizado com sucesso!")
+
+        else:
+            print("Valor inválido!")
+
+    # Transferir
+    def transferir(self, valor, conta_destino):
+
+        if self.sacar(valor):
+            conta_destino.depositar(valor)
+            print("Transferência realizada com sucesso!")
+
+        else:
+            print("Transferência não realizada!")
