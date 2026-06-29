76beeb5df20c88bf5c85c2f6084e372a77e27479
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-06-28 21:56:24 -0300

    Criar teste-funcionamento.py

---
+++ segundo-tri/teste-funcionamento.py
@@ -0,0 +1,11 @@
+from conta import Conta
+
+contaPatrick = Conta("Patrick", "123.456.789-00", 500, "1234", 5678)
+contaPatrick.mostrar()
+
+# criar os seguintes metodos
+contaPatrick.depositar(1000)
+contaPatrick.sacar(250)
+
+conta2 = Conta("Fulano", "987.654.321-00", 500, "3333", 2314)
+contaPatrick.pix(conta2, 250)
